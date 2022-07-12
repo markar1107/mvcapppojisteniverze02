79bce677300b889cca0a913a75fc23133c5611c8
Johny Finger <Johny Finger@DESKTOP-Q40F4QA>
2022-07-12 19:11:00 +0200

    úprava View přidat pojištění

--- Models/ZaznamPojisteni.cs
+++ Models/ZaznamPojisteni.cs
@@ -6,11 +6,14 @@ namespace mvcapppojisteniverze02.Models
     {
         public int ZaznamPojisteniID { get; set; }
         public int KlientID { get; set; }
+        [Display(Name = "Produkt")]
         public int ProduktID { get; set; }
         public int Cena { get; set; }
+        [Display(Name ="Začátek pojištění")]
         [DataType(DataType.Date)]
         public DateTime ZacatekPojisteni { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Konec Pojištění")]
         public DateTime KonecPojisteni { get; set; }
 
         public Klient Klient { get; set; }
