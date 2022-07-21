e50d7fafd0d31ad8da6544c782ec9012c52b4fe9
Johny Finger <Johny Finger@DESKTOP-Q40F4QA>
2022-07-21 06:07:52 +0200

    finální verze k prezentaci

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -13,8 +13,11 @@ namespace mvcapppojisteniverze02.Data
 
             var klienti = new Klient[]
             {
-                new Klient{Jmeno="Martin", Prijmeni="Kára",Telefon=123,Email="martin.kara@volny.cz",Ulice="Lábkova 25",Mesto="Plzeň",Psc="318 06"},
-                new Klient{Jmeno="Aleš",Prijmeni="Bláha",Telefon=456,Email="blaha.ales@seznam.cz",Ulice="Slovanská 254", Mesto="Plzeň", Psc="316 00"},
+                new Klient{Jmeno="Martin", Prijmeni="Kára", Telefon=608960709, Email="martin.kara@volny.cz", Ulice="Lábkova 25", Mesto="Plzeň", Psc="318 06"},
+                new Klient{Jmeno="Aleš", Prijmeni="Bláha", Telefon=734256825, Email="blaha.ales@seznam.cz", Ulice="Slovanská 254", Mesto="Plzeň", Psc="316 00"},
+                new Klient{Jmeno="Lenka", Prijmeni="Pejčeva", Telefon=728287956, Email="lenka.pejceva@seznam.cz", Ulice="U lesa 30", Mesto="Drmoul", Psc="456 01"},
+                new Klient{Jmeno="Pavel", Prijmeni="Kaucký", Telefon=602562456, Email="kaucky.pavel@gmail.com", Ulice="Slovanských hrdinů 354", Mesto="Praha", Psc="256 41"},
+                new Klient{Jmeno="Robert", Prijmeni="Zubkovský", Telefon=754256489, Email="zubkovsky.robert@tiscali.cz", Ulice="Drážďanská 452", Mesto="Praha", Psc="254 01"},
             };
 
             context.Klienti.AddRange(klienti);
@@ -22,8 +25,13 @@ namespace mvcapppojisteniverze02.Data
 
             var produkty = new Produkt[]
             {
-                new Produkt{Nazev="Pojištění domácnosti",Popis="základní pojištění bytu"},
-                new Produkt{Nazev="Autopojištění", Popis="standardní pojištění osobního auta do 3500kg"},
+                new Produkt{Nazev="Pojištění domácnosti",Popis="základní pojištění bytu nebo rodinného domu"},
+                new Produkt{Nazev="Autopojištění osobních vozidel", Popis="standardní pojištění osobního vozidla do 3500kg"},
+                new Produkt{Nazev="Autopojištění nákladních vozidel a přívěsů", Popis="standardní pojištění nákladních vozidel nad 3500kg"},
+                new Produkt{Nazev="Živostní pojištění", Popis="pojištění mimořádné události úmrtí či poškození zdraví"},
+                new Produkt{Nazev="Cestovní pojištění", Popis="pojištění rizik ztráty majetku nebo úrazu při cestování v rámci EU"},
+                new Produkt{Nazev="Úrazové pojištění", Popis="pojištění proti úrazu při výkonu zaměstnání či jiné mimopracovní aktivity"},
+                new Produkt{Nazev="Pojištění rizik spojených s výkonem podnikatelské činnosti", Popis="pojištění proti neočekávané události omezující podnikání či proti úpadku"},
             };
 
             context.Produkty.AddRange(produkty);
@@ -31,8 +39,14 @@ namespace mvcapppojisteniverze02.Data
 
             var zaznamyPojisteni = new ZaznamPojisteni[]
             {
-                new ZaznamPojisteni{KlientID=1,ProduktID=1,PredmetPojisteni="byt",Cena=1000,ZacatekPojisteni=DateTime.Parse("2022/02/05"),KonecPojisteni=DateTime.Parse("2023/02/04")},
-                new ZaznamPojisteni{KlientID=2,ProduktID=2,PredmetPojisteni="osobní auto",Cena=3500,ZacatekPojisteni=DateTime.Parse("2021/08/16"),KonecPojisteni=DateTime.Parse("2022/08/15")},
+                new ZaznamPojisteni{KlientID=1, ProduktID=1, PredmetPojisteni="byt v osobním vlastnictví", Cena=1000, ZacatekPojisteni=DateTime.Parse("2022/02/05"), KonecPojisteni=DateTime.Parse("2023/02/04")},
+                new ZaznamPojisteni{KlientID=1, ProduktID=6, PredmetPojisteni="zdraví osoby", Cena=530, ZacatekPojisteni=DateTime.Parse("2021/08/12"), KonecPojisteni=DateTime.Parse("2022/08/11")},
+
+                new ZaznamPojisteni{KlientID=2 ,ProduktID=2 ,PredmetPojisteni="osobní auto Opel Astra", Cena=3500, ZacatekPojisteni=DateTime.Parse("2021/08/16"), KonecPojisteni=DateTime.Parse("2022/08/15")},
+                new ZaznamPojisteni{KlientID=3, ProduktID=7, PredmetPojisteni="kavárna", Cena=8400, ZacatekPojisteni=DateTime.Parse("2022/01/05"), KonecPojisteni=DateTime.Parse("2023/01/04")},
+                new ZaznamPojisteni{KlientID=4, ProduktID=5, PredmetPojisteni="život osoby", Cena=580, ZacatekPojisteni=DateTime.Parse("2022/02/14"), KonecPojisteni=DateTime.Parse("2023/08/25")},
+                new ZaznamPojisteni{KlientID=5, ProduktID=3, PredmetPojisteni="nákladní auto Mercedes", Cena=8500, ZacatekPojisteni=DateTime.Parse("2021/04/25"), KonecPojisteni=DateTime.Parse("2023/04/24")},
+
 
             };
 
--- Models/Klient.cs
+++ Models/Klient.cs
@@ -4,7 +4,9 @@ namespace mvcapppojisteniverze02.Models
 {
     public class Klient
     {
+       
         public int ID { get; set; }
+
         [Display(Name = "Jméno")]
         [Required(ErrorMessage = "Vyplňte jméno")]
         [StringLength(50, ErrorMessage = "Jméno je příliš dlouhé, max.50 znaků")]
@@ -12,29 +14,29 @@ namespace mvcapppojisteniverze02.Models
         [Display(Name = "Příjmení")]
         [Required(ErrorMessage = "Vyplňte příjmení")]
         [StringLength(50, ErrorMessage = "Příjmení je příliš dlouhé, max.50 znaků")]
-
         public string Prijmeni { get; set; }
         public int Telefon { get; set; }
+
         [DataType(DataType.EmailAddress)]
         [EmailAddress(ErrorMessage = "Neplatný email")]
         public string Email { get; set; }
+
         [Display(Name = "Ulice a č.popisné")]
         [Required(ErrorMessage = "Vyplňte adresu")]
         [StringLength(60, ErrorMessage = "Název ulice je příliš dlouhý, max.60 znaků")]
-
         public string Ulice { get; set; }
+
         [Display(Name = "Město")]
         [Required(ErrorMessage = "Vyplňte adresu")]
         [StringLength(50, ErrorMessage = "Název města je příliš dlouhý, max.50 znaků")]
-
         public string Mesto { get; set; }
+
         [Display(Name = "PSČ")]
         [Required(ErrorMessage = "Vyplňte adresu")]
-        [StringLength(5, ErrorMessage = "PSČ je příliš dlouhé, max.5 znaků")]
-
+        [StringLength(6, ErrorMessage = "PSČ je příliš dlouhé, max.6 znaků")]
         public string Psc { get; set; }
+
         [Display(Name = "Sjednaná pojištění")]
-        
         public ICollection<ZaznamPojisteni> ZaznamPojisteniKolekce { get; set; }
     }
 }
